fa5d547652960750c2dcc40b5bb34e620d55e997
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-08 20:00:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!!  Did i really though? what is life if i am just a pawn? "
+    "Started my server using Shotgun? "
     
   end
 
