ae2eb9f7534b0165b21a339842ce4626f29ae181
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-08 19:59:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!!  I BUILT THIS!"
+    "Welcome to your app!!!!  Did i really though? what is life if i am just a pawn? "
     
   end
 
